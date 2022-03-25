fd35bbfaac3cfffdda8edfb36172d08b7f504f64
Nihisil <alexey@nihisil.com>
2022-03-25 13:10:23 +0800

    Improve tests coverage, ref #8

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -3,4 +3,8 @@
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+
+  config.before(:each, devise_mapping: true) do
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+  end
 end
